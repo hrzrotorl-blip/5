676ba2dab57c0cb61494e2abc7320b5762f14940
hrzrotorl-blip <hrzrotorl@gmail.com>
2025-10-27 17:26:22 +0900

    마우스포인트다시활성

--- Assets/Title/Title Script/ReturnToTitle.cs
+++ Assets/Title/Title Script/ReturnToTitle.cs
@@ -7,20 +7,30 @@ public class ReturnToTitle : MonoBehaviour
 {
     // 메인 타이틀 씬 이름 (씬 이름 정확히 입력!)
     public string titleSceneName = "MainTitle";
+    private static ReturnToTitle instance;
+
+    void Awake()
+    {
+        // 이미 인스턴스가 존재하면 자신은 제거
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        // 처음 만들어진 인스턴스라면 유지
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
 
     void Update()
     {
         // ESC 키 입력 감지
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.P))
         {
             // 메인 타이틀로 전환
             SceneManager.LoadScene(titleSceneName);
+            Cursor.lockState = CursorLockMode.Confined;
         }
-    }
-
-    // 씬이 바뀌어도 이 오브젝트가 파괴되지 않도록 설정
-    private void Awake()
-    {
-        DontDestroyOnLoad(gameObject);
     }
 }
